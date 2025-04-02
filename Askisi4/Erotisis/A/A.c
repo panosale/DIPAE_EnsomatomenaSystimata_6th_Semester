c5c78a064d309bd58ddc3cd2f4f1c6f706da8ee1
panosale <panosalevr@hotmail.gr>
2025-04-02 23:38:24 +0300

    .

---
+++ Askisi4/Erotisis/A/A.c
@@ -0,0 +1,28 @@
+// DIPAE - Enosmatomena Systimata - 6o Eksamino
+// Alevropoulos Panagiotis
+// AM: 2022005
+// Erotisi A
+#include <main_.h>
+
+#use standard_io (A) // Standard Eisodoi & Eksodoi
+#use standard_io (B) // Standard Eisodoi & Eksodoi
+#use standard_io (C) // Standard Eisodoi & Eksodoi
+
+#byte PORTA =0xF80 // Orismos ton thiron me ti thesi tous sti mnimi
+#byte PORTB =0xF81 // Orismos ton thiron me ti thesi tous sti mnimi
+#byte PORTC =0xF82 // Orismos ton thiron me ti thesi tous sti mnimi
+#byte PORTD =0xF83 // Orismos ton thiron me ti thesi tous sti mnimi
+#byte PORTE =0xF84 // Orismos ton thiron me ti thesi tous sti mnimi
+
+void main()
+{
+   set_tris_b(0x00); // Orismos ton tou PORTB san eksodo (0)
+   set_tris_a(0xff); // Orismos ton tou PORTA san eisodo (1)
+   PORTB = 0;
+   while (TRUE) {
+      output_bit(PIN_B0, bit_test(PORTA, 1));
+   }
+}   
+
+void init(void) {
+}
